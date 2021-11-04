f838c6ee191481fa348609c2e284aebb39e75e48
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-04 16:56:41 +0100

    parte bonus terminata e funzionante

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -41,25 +41,9 @@ const roleForm = document.getElementById('role');
 const imageForm = document.getElementById('image');
 const addMemberButton = document.getElementById('addMemberButton');
 
-// aggiungiamo tutte le card all'inteno dell html con un for sull array di ogj
-let newCard = '';
-for (let i=0; i<teams.length; i++){
-    newCard +=`
-        <div class="team-card">
-            <div class="card-image">
-                <img
-                    src="${teams[i].imgCard}"
-                    alt="${teams[i].nameCard}"
-            />
-            </div>
-            <div class="card-text">
-                <h3>${teams[i].nameCard}</h3>
-                <p>${teams[i].roleCard}</p>
-            </div>
-        </div>
-    `;
-}
-teamContainer.innerHTML += newCard;
+newCardItem(teams);
+
+
 
 // estrapoliamo gli input dal form per creare una nuova card ed inserirla nell html
 addMemberButton.addEventListener('click',
@@ -70,11 +54,54 @@ addMemberButton.addEventListener('click',
             'roleCard' : roleForm.value 
         };
         teams.push(newObj);
+
+        let newItem = '';
+        newItem =`
+            <div class="team-card">
+                <div class="card-image">
+                    <img
+                        src="${imageForm.value}"
+                        alt="${nameForm.value}"
+                />
+                </div>
+                <div class="card-text">
+                    <h3>${nameForm.value}</h3>
+                    <p>${roleForm.value}</p>
+                </div>
+            </div>
+        `;
+        teamContainer.innerHTML += newItem;
+
         
         // stampa in console l'array di obj
         for (let key in teams){
             console.log(teams[key]);
         }
+    
+        return newItem
     }
 );
+
+// Funzioni
+// aggiungiamo tutte le card all'inteno dell html con un for sull array di ogj
+function newCardItem(objArr){
+    let newCard = '';
+    for (let i=0; i<objArr.length; i++){
+        newCard +=`
+            <div class="team-card">
+                <div class="card-image">
+                    <img
+                        src="${objArr[i].imgCard}"
+                        alt="${objArr[i].nameCard}"
+                />
+                </div>
+                <div class="card-text">
+                    <h3>${objArr[i].nameCard}</h3>
+                    <p>${objArr[i].roleCard}</p>
+                </div>
+            </div>
+        `;
+    }
+    teamContainer.innerHTML += newCard;
+}
 
